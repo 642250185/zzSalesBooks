f49bc813c04f106cf58f0ee6bd6478884d0c220e
642250185 <nihao642250185@163.com>
2018-08-29 17:41:37 +0800

    转转销售端所有书籍数据

--- config/index.js
+++ config/index.js
@@ -4,11 +4,10 @@ const config = {
     zz: {
         PPU: 'TT=e455cbe40786afc0913de961956d0530ee5a8341&UID=46028783232276&SF=ZHUANZHUAN&SCT=1535112154426&V=1&ET=1537700554426',
         domain: 'https://zhuan.58.com',
-        openRoute: '/zzopen/book',
-        addCartPath: '/getBook',
-        bookCartListPath: '/getBookCartList?activityId=10003',
-        delRecyclePath: '/delRecycleBookCart',
-        bookDataPath : path.join(__dirname, '..', 'data/booksPrice.json'),
+        zzopenRoute: '/zzopen/sellbook',
+        homePage: '/homePage?fromChannel=ppzq',
+        bookListByCate: '/bookListByCate',
+        categoryPath : path.join(__dirname, '..', 'data/category.json'),
         exportPath: path.join(__dirname, '..', 'download'),
     },
     category: {
